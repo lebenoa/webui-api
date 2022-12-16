4b204c1dffb7e0c7cfadef72247bcea3823f6d0d
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-16 14:56:17 +0700

    Add: ReadFromFileIgnore & Comment
    Add:
    - ReadFromFileIgnore
        Which is the same as ReadFromFile but ignore any errors
    - Function description

--- img2img/helper.go
+++ img2img/helper.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Meonako/webui-api/utils"
 )
 
+// Convenient function to encode image in any format to base64
 func ReadFromFile(path string) (string, error) {
 	fileBytes, err := os.ReadFile(path)
 	if err != nil {
@@ -18,6 +19,14 @@ func ReadFromFile(path string) (string, error) {
 	return ReadFromBytes(fileBytes)
 }
 
+// Convenient function to encode image in any format to base64 and IGNORE ANY ERRORS that might occur.
+func ReadFromFileIgnore(path string) string {
+	fileBytes, _ := os.ReadFile(path)
+	data, _ := ReadFromBytes(fileBytes)
+	return data
+}
+
+// Convenient function to encode image data in bytes format to base64.
 func ReadFromBytes(data []byte) (string, error) {
 	img, _, err := image.Decode(bytes.NewReader(data))
 	if err != nil {
