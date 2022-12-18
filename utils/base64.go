0187bec3448f82cbc0512c74c582ae0f1edd6512
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-18 23:27:18 +0700

    Move & Simpliflies: base64 helper
    Update:
       - Move: helper function to utils package
          cuz you need this to others too
       - Rename: function start with "Read" to "Base64"
          cuz it might misleading
           e.g. "ReadFromFile" to "Base64FromFile"
        - Remove unnecessary code

--- utils/base64.go
+++ utils/base64.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"os"
 	"encoding/base64"
 )
 
@@ -10,4 +11,16 @@ func EncodeBase64(data []byte) string {
 
 func DecodeBase64(data string) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(data)
+}
+
+// Convenience function to encode image in any format to base64
+func Base64FromFile(path string) (string, error) {
+	fileBytes, err := os.ReadFile(path)
+	return EncodeBase64(fileBytes), err
+}
+
+// Convenience function to encode image in any format to base64 and IGNORE ANY ERRORS that might occur.
+func Base64FromFileIgnore(path string) string {
+	fileBytes, _ := os.ReadFile(path)
+	return EncodeBase64(fileBytes)
 }
