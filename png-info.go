90a39b24580f422ca7922dca6a512eb05a78e7d8
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-19 01:06:20 +0700

    Add: more debug info

--- png-info.go
+++ png-info.go
@@ -11,10 +11,13 @@ func (a *api) PNGInfo(image string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	fmt.Println(payload)
 
 	data, err := a.post(a.Config.Path.PNGInfo, payload)
 	if err != nil {
 		return "", err
+	} else if string(data) == "Internal Server Error" {
+		return "", fmt.Errorf("%v", "500 internal server error")
 	}
 
 	var result map[string]interface{}
