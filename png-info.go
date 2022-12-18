3dfcd444ce4e514f69b89f41624a47bf4f48e482
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-19 00:35:40 +0700

    Add: PNG Info API

---
+++ png-info.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/goccy/go-json"
+)
+
+func (a *api) PNGInfo(image string) (string, error) {
+	payload, err := json.Marshal(map[string]string{"image": image})
+	if err != nil {
+		return "", err
+	}
+
+	data, err := a.post(a.Config.Path.PNGInfo, payload)
+	if err != nil {
+		return "", err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return "", err
+	}
+
+	info, ok := result["info"].(string)
+	if !ok {
+		return "", fmt.Errorf("%v", string(data))
+	}
+
+	return info, err
+}
