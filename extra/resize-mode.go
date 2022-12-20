d6d9f287e92985f3bfa855f3cadac38eeea612da
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-20 18:29:05 +0700

    Add: Comment/Description

--- extra/resize-mode.go
+++ extra/resize-mode.go
@@ -2,7 +2,7 @@ package extra
 
 type resizeMode struct {
 	SCALE_BY int // Multiplier to width and height of the original image
-	SCALE_TO int //
+	SCALE_TO int // Scale to specify width and height
 }
 
 var ResizeMode = resizeMode{
