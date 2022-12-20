ddec48245ea7f50decb0bf391ff08826e3a71ee2
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-20 18:30:00 +0700

    Little Touchup

--- extra-single-resp.go
+++ extra-single-resp.go
@@ -3,7 +3,7 @@ package api
 import "github.com/Meonako/webui-api/utils"
 
 type extraSingleImageRespond struct {
-	HTMLInfo     string `json:"html_info"` // Upscaler info in HTML format.
+	HTMLInfo     string `json:"html_info"` // Upscaler info in HTML format. I don't even know why they return HTML format for API
 	Image        string `json:"image"`     // Base64-encoded image
 	DecodedImage []byte // Base64-decoded image data in byte
 }
--- extra/const.go
+++ extra/const.go
@@ -1,8 +1,11 @@
 package extra
 
 const (
-	ScaleBy = iota
-	ScaleTo
+	ScaleBy = iota // Multiplier to width and height of the original image
+	ScaleTo        // Scale to specify width and height
+
+	SCALE_BY = ScaleBy // Multiplier to width and height of the original image
+	SCALE_TO = ScaleTo // Scale to specify width and height
 )
 
 const (
