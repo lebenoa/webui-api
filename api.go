68f292475a8c883bfe5ba0100d5f290a16a7994d
Meonako <76484203+Meonako@users.noreply.github.com>
2022-12-27 18:11:12 +0700

    Add: Auth for API **DIDNT TEST**

--- api.go
+++ api.go
@@ -10,7 +10,20 @@ import (
 
 type api struct {
 	Config Config
+
+	//  Auth contains username and password like this:
+	//
+	//   "username, password" or you can use Username && Password field instead.
+	//
+	//  But do keep in mind that if Auth is not empty string, it'll use Auth and not Username && Password
+	Auth     string
+	Username string
+	Password string
 }
+
+var (
+	httpClient = &http.Client{}
+)
 
 func New(newConfig ...Config) *api {
 	if len(newConfig) <= 0 {
@@ -24,6 +37,17 @@ func New(newConfig ...Config) *api {
 	}
 }
 
+// Set username and password for use when making request. Equivalent to
+//
+//	api.Username = username
+//	api.Password = password
+//
+// Either username or password should not be empty string
+func (a *api) SetAuth(username, password string) {
+	a.Username = username
+	a.Password = password
+}
+
 // Convenience function to build prompt.
 //
 // BuildPrompt("masterpiece", "best quality", "solo") -> "masterpiece, best quality, solo"
@@ -31,25 +55,55 @@ func BuildPrompt(args ...string) string {
 	return strings.Join(args, ", ")
 }
 
+// Send Get Request.
 func (a *api) get(path string) (body []byte, err error) {
-	resp, err := http.Get(a.Config.BaseURL + path)
+	req, err := http.NewRequest("GET", a.Config.BaseURL+path, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return common(resp)
+	a.setAuth(req)
+
+	return a.exec(req)
 }
 
+// Send Post Request.
 func (a *api) post(path string, data []byte) (body []byte, err error) {
-	resp, err := http.Post(a.Config.BaseURL+path, "application/json", bytes.NewReader(data))
+	req, err := http.NewRequest("POST", a.Config.BaseURL+path, bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
 
-	return common(resp)
+	req.Header.Set("Content-Type", "application/json")
+	a.setAuth(req)
+
+	return a.exec(req)
 }
 
-func common(resp *http.Response) (body []byte, err error) {
+// Set HTTP Basic Auth.
+func (a *api) setAuth(req *http.Request) {
+	if a.Auth != "" {
+		credit := strings.Split(a.Auth, ", ")
+		req.SetBasicAuth(credit[0], credit[1])
+	} else if a.Username != "" && a.Password != "" {
+		req.SetBasicAuth(a.Username, a.Password)
+	}
+}
+
+// Send Request.
+func (a *api) exec(req *http.Request) ([]byte, error) {
+	a.setAuth(req)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return readBody(resp)
+}
+
+// Read response body.
+func readBody(resp *http.Response) (body []byte, err error) {
 	defer resp.Body.Close()
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
