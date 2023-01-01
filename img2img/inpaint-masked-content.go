928fb0d9f384ab28689c3042fe0664a4987672be
Meonako <76484203+Meonako@users.noreply.github.com>
2023-01-01 09:32:57 +0700

    Add: More helper exported
    - Bring back const
    - Export both camelCase and ALL_CAPS

--- img2img/inpaint-masked-content.go
+++ img2img/inpaint-masked-content.go
@@ -1,6 +1,23 @@
 package img2img
 
+const (
+	Fill          = iota // fill it with colors of the image.
+	Original             // keep whatever was there originally.
+	LatentNoise          // fill it with latent space noise.
+	LatentNothing        // fill it with latent space zeroes.
+
+	FILL           = Fill          // fill it with colors of the image.
+	ORIGINAL       = Original      // keep whatever was there originally.
+	LATENT_NOISE   = LatentNoise   // fill it with latent space noise.
+	LATNET_NOTHING = LatentNothing // fill it with latent space zeroes.
+)
+
 type inpainMaskContent struct {
+	Fill          int // fill it with colors of the image.
+	Original      int // keep whatever was there originally.
+	LatentNoise   int // fill it with latent space noise.
+	LatentNothing int // fill it with latent space zeroes.
+
 	FILL           int // fill it with colors of the image.
 	ORIGINAL       int // keep whatever was there originally.
 	LATENT_NOISE   int // fill it with latent space noise.
@@ -8,8 +25,13 @@ type inpainMaskContent struct {
 }
 
 var INPAINT_MASK_CONENT = inpainMaskContent{
-	FILL:           0,
-	ORIGINAL:       1,
-	LATENT_NOISE:   2,
-	LATNET_NOTHING: 3,
+	Fill:          Fill,
+	Original:      Original,
+	LatentNoise:   LatentNoise,
+	LatentNothing: LatentNothing,
+
+	FILL:           Fill,
+	ORIGINAL:       Original,
+	LATENT_NOISE:   LatentNoise,
+	LATNET_NOTHING: LatentNothing,
 }
--- img2img/inpaint-mode.go
+++ img2img/inpaint-mode.go
@@ -1,11 +1,25 @@
 package img2img
 
+const (
+	InpaintMaskedArea        = iota // Inpaint the masked area.
+	InpaintOutsideMaskedArea        // Inpaint outside the masked area/everything except for masked area.
+
+	INPAINT_MASKED_AREA         = InpaintMaskedArea        // Inpaint the masked area.
+	INPAINT_OUTSIDE_MASKED_AREA = InpaintOutsideMaskedArea // Inpaint outside the masked area/everything except for masked area.
+)
+
 type inpaintMode struct {
+	InpaintMaskedArea        int // Inpaint the masked area.
+	InpaintOutsideMaskedArea int // Inpaint outside the masked area/everything except for masked area.
+
 	INPAINT_MASKED_AREA         int // Inpaint the masked area.
 	INPAINT_OUTSIDE_MASKED_AREA int // Inpaint outside the masked area/everything except for masked area.
 }
 
 var INPAINT_MODE = inpaintMode{
-	INPAINT_MASKED_AREA:         0,
-	INPAINT_OUTSIDE_MASKED_AREA: 1,
+	InpaintMaskedArea:        InpaintMaskedArea,
+	InpaintOutsideMaskedArea: InpaintOutsideMaskedArea,
+
+	INPAINT_MASKED_AREA:         InpaintMaskedArea,
+	INPAINT_OUTSIDE_MASKED_AREA: InpaintOutsideMaskedArea,
 }
